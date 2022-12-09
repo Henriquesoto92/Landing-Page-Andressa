f97af4a1d67fa9142616502a0c8c98317b0665c6
Henriquesoto92 <henriquesoto92@gmail.com>
2022-12-09 20:24:51 -0300

    :construction: work in Header

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,9 +1,69 @@
-import { Container, Flex } from "@mantine/core";
+import { Card, Container, createStyles, Flex, Image } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+
+const useStyles = createStyles((theme, _params, getRef) => ({
+  price: {
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+  },
+
+  carousel: {
+    "&:hover": {
+      [`& .${getRef("carouselControls")}`]: {
+        opacity: 1,
+      },
+    },
+  },
+
+  carouselControls: {
+    ref: getRef("carouselControls"),
+    transition: "opacity 150ms ease",
+    opacity: 0,
+  },
+
+  carouselIndicator: {
+    width: 4,
+    height: 4,
+    transition: "width 250ms ease",
+
+    "&[data-active]": {
+      width: 16,
+    },
+  },
+}));
+
+const images = [
+  "https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+  "https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+  "https://images.unsplash.com/photo-1605774337664-7a846e9cdf17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+  "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+  "https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+];
 
 function Headers() {
+  const { classes } = useStyles();
+
+  const slides = images.map((image) => (
+    <Carousel.Slide key={image}>
+      <Image src={image} height={220} />
+    </Carousel.Slide>
+  ));
+
   return (
     <Container bg="yellow.1">
       <Flex h="300px" w="full">
+        <Card.Section>
+          <Carousel
+            withIndicators
+            loop
+            classNames={{
+              root: classes.carousel,
+              controls: classes.carouselControls,
+              indicator: classes.carouselIndicator,
+            }}
+          >
+            {slides}
+          </Carousel>
+        </Card.Section>
         Headers
       </Flex>
     </Container>
