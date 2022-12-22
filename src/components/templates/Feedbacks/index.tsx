9b67d364b7238ce04e254599eb108afa932081aa
Henriquesoto92 <henriquesoto92@gmail.com>
2022-12-22 20:37:54 -0300

    :construction: work in progress

--- src/components/templates/Feedbacks/index.tsx
+++ src/components/templates/Feedbacks/index.tsx
@@ -1,12 +1,63 @@
-import { Container, Flex } from "@mantine/core";
-import ButtonEspecial from "../../atoms/ButtomEspecial";
+import {
+  Avatar,
+  Card,
+  Container,
+  Flex,
+  Grid,
+  Image,
+  Text,
+  Title,
+} from "@mantine/core";
 
 function Feedbacks() {
+  const specialyts = [
+    {
+      name: "Magda",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80",
+      description:
+        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      name: "Neusa",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80",
+      description:
+        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      name: "Miriam",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80",
+      description:
+        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      name: "Andressa",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80",
+      description:
+        "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+  ];
+
   return (
-    <Container bg="yellow.1">
-      <Flex h="300px" w="full">
-        Feedbacks
-        <ButtonEspecial text="Feedbacks" />
+    <Container bg="teal.0" maw="1920px" p="20px 200px">
+      <Flex w="full" direction="row" align="center" gap="40px">
+        {specialyts.map((specialyt, index) => (
+          <Card
+            key={`${specialyt.name}${index}`}
+            shadow="sm"
+            radius="sm"
+            bg="white"
+          >
+            <Flex align="center" gap="20px">
+              <Avatar radius="xl" size="xl" src={specialyt.image} />
+              <Title>{specialyt.name}</Title>
+            </Flex>
+            <Text ta="center">"{specialyt.description}"</Text>
+          </Card>
+        ))}
       </Flex>
     </Container>
   );
